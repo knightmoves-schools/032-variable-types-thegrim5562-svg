65733bf0a880a528ee0d8ffff75f9a380d00ab5b
raishaun cooke <thegrim5562@gmail.com>
2026-04-28 10:22:36 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,3 @@ let height = 6.5;
 let isIndoorPet = true;
 
 document.getElementById("string").innerHTML = typeof animalName;
-document.getElementById("number").innerHTML = typeof height;
-document.getElementById("boolean").innerHTML = typeof isIndoorPet;
