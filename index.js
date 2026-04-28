737bde4496f97a722d91022252f5706b8cc8151c
raishaun cooke <thegrim5562@gmail.com>
2026-04-28 10:20:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+let animalName = "panda";
+let height = 6.5;
+let isIndoorPet = true;
 
+document.getElementById("string").innerHTML = typeof animalName;
+document.getElementById("number").innerHTML = typeof height;
+document.getElementById("boolean").innerHTML = typeof isIndoorPet;
